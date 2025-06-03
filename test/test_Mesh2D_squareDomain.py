ecfaa02621ad64708cd0ea89e1709b124ebc5696
Theo Girard <girard-theo@orange.fr>
2025-06-03 17:37:53 +0200

    coverage 34-38%

--- test/test_Mesh2D_squareDomain.py
+++ test/test_Mesh2D_squareDomain.py
@@ -83,8 +83,7 @@ def test_NCD_hasExitEdge(edge, result):
 def test_NCD_hasOuterEdge(edge, result):
     assert Domain1.hasOuterEdge(edge) == result
 
-#Plotting method to check the resulting domain:
-#Domain1.show()
+
 
 #Tests for the Mesh object ----------------------------------------------------
 
@@ -103,3 +102,82 @@ def test_Mesh_zones():
 
     Mesh1.computeZonesTriangles()
     assert len(Mesh1.zones["Left side"]['triangles']) > 0
+
+def test_generateMesh():
+    assert max(Mesh1.cellAreas) <  0.1
+
+def test_saveLoadJson():
+    Mesh1.saveToJson("test/testMesh1")
+    Mesh2 = Mesh()
+    Mesh2.loadFromJson("test/testMesh1_mesh.json")
+
+    for i in range(len(Mesh1.triangles)):
+        for j in range(3):
+            assert Mesh1.triangles[i][j] == Mesh2.triangles[i][j]
+
+def test_exits():
+    for indexP in Mesh1.exitVertices:
+        assert Domain1.hasExitPoint(Mesh1.vertices[indexP])
+    for edge in Mesh1.exitEdges:
+        exit = [    [Mesh1.vertices[Mesh1.edges[edge][0]][0], Mesh1.vertices[Mesh1.edges[edge][0]][1]],
+                    [Mesh1.vertices[Mesh1.edges[edge][1]][0], Mesh1.vertices[Mesh1.edges[edge][1]][1]] ]
+        assert Domain1.hasExitEdge(exit)
+
+def test_walls():
+    for indexP in Mesh1.boundaryPoints:
+        assert Domain1.hasBoundaryPoint(Mesh1.vertices[indexP]) or Domain1.hasWallPoint(Mesh1.vertices[indexP])
+    for edge in Mesh1.wallEdges:
+        wall = [    [Mesh1.vertices[Mesh1.edges[edge][0]][0], Mesh1.vertices[Mesh1.edges[edge][0]][1]],
+                    [Mesh1.vertices[Mesh1.edges[edge][1]][0], Mesh1.vertices[Mesh1.edges[edge][1]][1]] ]
+        assert Domain1.hasWallEdge(wall) or Domain1.hasOuterEdge(wall)
+
+
+
+#Tests for the CellValueMap object--------------------------------------------
+Map1 = CellValueMap(Mesh1)
+
+for i in range(len(Map1)):
+    Map1[i] = 0.1
+
+def test_algebra_cellvaluemap():
+    Map2 = CellValueMap(Mesh1)
+    for i in range(len(Map2)):
+        Map2[i] = 0.3
+    Map3 = Map1*2 + Map2
+    for i in range(len(Map3)):
+        assert Map3[i] == 0.5
+    Map3 += 0.25
+    print(type(Map3))
+    for i in range(len(Map3)):
+        assert Map3[i] == 0.75
+
+def test_sets_cellvaluemap():
+    Map2 = CellValueMap(Mesh1)
+    for i in range(len(Map2)):
+        assert Map2[i] == 0
+    Map2.generateRandom(variability = 0.4,mean = 0.3)
+    for i in range(len(Map2)):
+        assert Map2[i] > 0
+
+    Map1.setConstantCircle(center=[1,1], radius = 1, value = 0.5)
+    for i in range(len(Map1)):
+        if np.sqrt( (Mesh1.barycenters[i][0]-1)**2 + (Mesh1.barycenters[i][1]-1)**2 ) < 1:
+            assert Map1[i] == 0.5
+        else:
+            assert Map1[i] == 0.1
+
+def test_convolutionOverSquareBall():
+    Mesh2 = Mesh()
+    Mesh2.generateMeshFromDomain(Domain1, 0.02,da=10)
+    Map = CellValueMap(Mesh2)
+    for i in range(len(Map)):
+        Map[i] = 2
+    Result = Map.convolutionOverSquareBall(radius=0.2, conv_func = (lambda x,y,z:2))
+    for i in range(len(Mesh2.vertices)):
+        if (Mesh2.vertices[i][0] > 0.2 and Mesh2.vertices[i][0] < 4.8
+                and Mesh2.vertices[i][1] > 0.2 and Mesh2.vertices[i][1] < 4.8
+                and (Mesh2.vertices[i][0] < 1.8 or Mesh2.vertices[i][0] > 3.2)
+                and (Mesh2.vertices[i][1] < 1.8 or Mesh2.vertices[i][1] > 3.2)) :
+            assert np.abs(Result[i] - 2*0.4**2) < float(1e-1)
+        else:
+            assert Result[i] < 2*0.4**2 + float(1e-1)
