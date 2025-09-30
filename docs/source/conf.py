34cc7dbdf4764b6ca3af17721857c89fa9384578
Théo Girard <47026669+TheoRGirard@users.noreply.github.com>
2025-09-30 12:04:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
 ]
 
 intersphinx_mapping = {
@@ -33,6 +34,22 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 autodoc_mock_imports = ['numpy', 'triangle', 'ezdxf', 'meshio', 'matplotlib', 'plotly']
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
 
 # -- Options for HTML output
 
